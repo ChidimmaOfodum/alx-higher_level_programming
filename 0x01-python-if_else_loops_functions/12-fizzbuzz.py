36612b984df6b536c1a3c3ab517f3ecfb982d2d2
Chidimma <chidimmamofodum@gmail.com>
2023-08-09 10:12:49 -0400

    fizzbuzz challenge

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        num = ""
+        if (i % 3 == 0):
+            num += "Fizz"
+        if i % 5 == 0:
+            num += "Buzz"
+        print(num or i, end=" ")
