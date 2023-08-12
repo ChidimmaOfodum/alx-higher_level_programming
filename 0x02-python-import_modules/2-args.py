ad085255614c585f73f74a58c60a0f39281a30a5
Chidimma <chidimmamofodum@gmail.com>
2023-08-11 21:44:12 -0400

    ensuring module is only called from the main function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
 
-length = len(sys.argv) - 1
-if (length == 0):
-    arguments = "arguments."
-else:
-    arguments = "argument:" if length == 1 else "arguments:"
+    length = len(sys.argv) - 1
+    if (length == 0):
+        arguments = "arguments."
+    else:
+        arguments = "argument:" if length == 1 else "arguments:"
 
-print("{:d} {}".format(length, arguments))
+    print("{:d} {}".format(length, arguments))
 
-if length > 0:
-    for i in range(1, length + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    if length > 0:
+        for i in range(1, length + 1):
+            print("{}: {}".format(i, sys.argv[i]))
