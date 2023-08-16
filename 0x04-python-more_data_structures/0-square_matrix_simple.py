c731da41e37e9a6ebaea82475a5f5c51f47488d5
Chidimma <chidimmamofodum@gmail.com>
2023-08-16 13:26:40 -0400

    squaring each element

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result =  list(map(lambda x: list(map(lambda y: y* 2, x)), matrix))
+    result =  list(map(lambda x: list(map(lambda y: y* y, x)), matrix))
     return result
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
