86fd4d05a550f4c2f48aefdb8b48c262f7d604ba
Chidimma <chidimmamofodum@gmail.com>
2023-08-17 20:08:25 -0400

    removing unnecessary print

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     max_key = list(a_dictionary.keys())[0]
     max_value = a_dictionary[max_key]
     for key, value in a_dictionary.items():
         if value > max_value:
             value, max_key = max_value, key
-    print(max_key)
     return max_key
