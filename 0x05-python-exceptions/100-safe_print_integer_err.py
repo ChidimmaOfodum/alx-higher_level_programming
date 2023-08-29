e1cd453456b35556668bfc0b389a6029258ca5bf
Chidimma <chidimmamofodum@gmail.com>
2023-08-28 22:21:53 -0400

    using correct stderr message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,23 @@ def safe_print_integer_err(value):
     try:
        print("{:d}".format(value))
     except Exception as ex:
-       sys.stderr.write(*(ex.args))
+       sys.stderr.write("Exception: " + ex.args[0])
        return False
     else:
        return True
+
+
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
