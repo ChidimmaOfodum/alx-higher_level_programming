9ffb4f96629ef40bd04d8bdfe8828fe940b21d49
Chidimma <chidimmamofodum@gmail.com>
2023-09-06 15:27:50 -0400

    removing underscores from width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """A rectangle class"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -15,9 +15,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if (type(self.__width) != int):
+        if (type(value) != int):
             raise TypeError("width must be an integer")
-        elif (self.__width < 0):
+        elif (value < 0):
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -28,9 +28,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if (type(self.__height) != int):
+        if (type(value) != int):
             raise TypeError("height must be an integer")
-        elif (self.__height < 0):
+        elif (value < 0):
+            print("less than zero")
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
