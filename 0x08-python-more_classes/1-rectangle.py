f99b270ff1d7717b24095976068012652743a3b4
Chidimma <chidimmamofodum@gmail.com>
2023-09-06 14:12:20 -0400

    defines a rectangle with private attributes, getter and setter

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+"""A module that defines a Rectangle class"""
+
+
+class Rectangle:
+    """A rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if (type(self.__width) != int):
+            raise TypeError("width must be an integer")
+        elif (self.__width < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if (type(self.__height) != int):
+            raise TypeError("height must be an integer")
+        elif (self.__height < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
