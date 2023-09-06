b7afe740818048f12f273987b1c9c12e2b3cc3aa
Chidimma <chidimmamofodum@gmail.com>
2023-09-06 17:41:07 -0400

    a custom __repr__ method

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""A module that defines a rectangle class"""
+
+
+class Rectangle:
+    """A rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        result = 0
+        if (self.width != 0 and self.height != 0):
+            result = 2 * (self.__width + self.__height)
+        return result
+
+    def __str__(self):
+        result = ""
+        if (self.width == 0 or self.height == 0):
+            return result
+        for i in range(self.height):
+            result += ("#" * self.width)
+            if (i != self.height - 1):
+                result += "\n"
+        return result
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
