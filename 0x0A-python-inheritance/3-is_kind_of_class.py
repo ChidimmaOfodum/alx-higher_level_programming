ac003659797679a2940408e414384b991078d50d
Chidimma <chidimmamofodum@gmail.com>
2023-09-11 16:07:06 -0400

    is instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Module that defines a function is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns whether a class is an instance of an object
+     Args:
+        obj (any): object
+        a_class (type): class
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    return isinstance(obj, a_class)
