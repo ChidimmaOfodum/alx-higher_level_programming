6aa2e9072fcca3328ac4e23b40922e2d084a8854
Chidimma <chidimmamofodum@gmail.com>
2023-09-11 13:10:23 -0400

    is instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" Module that defines a class Mylist"""
+
+
+def is_same_class(obj, a_class):
+    """returns whether a class is an instance of an object"""
+    return isinstance(obj, a_class)
