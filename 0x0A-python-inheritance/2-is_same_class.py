676016766e0f700358c006a7bdadc8532040b65d
Chidimma <chidimmamofodum@gmail.com>
2023-09-11 13:23:52 -0400

    obj has to be exactly/direct instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,5 +4,12 @@
 
 
 def is_same_class(obj, a_class):
-    """returns whether a class is an instance of an object"""
-    return isinstance(obj, a_class)
+    """returns whether a class is an instance of an object
+     Args:
+        obj (any): object
+        a_class (type): class
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    return type(obj) == a_class
