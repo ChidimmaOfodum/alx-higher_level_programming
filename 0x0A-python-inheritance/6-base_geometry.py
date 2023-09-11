4d956435a59eeb27dec3f49b46505e0626ad2d9e
Chidimma <chidimmamofodum@gmail.com>
2023-09-11 19:49:53 -0400

    a public instance method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A module that defines a class"""
+
+
+class BaseGeometry:
+    """A basegeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
