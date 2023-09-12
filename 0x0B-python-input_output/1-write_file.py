15e609a3bb82f24184d13360dd228046632ff97d
Chidimma <chidimmamofodum@gmail.com>
2023-09-12 14:10:17 -0400

    typo error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 """A module that defines write_file function"""
+
 
 def write_file(filename="", text=""):
     """A function that writes to a file"""
-
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
