96d30d4437998cb5eaee77dd2dc956d1e2e98ade
Chidimma <chidimmamofodum@gmail.com>
2023-09-12 17:29:27 -0400

    convert to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""A module that defines to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """A function that returns json representation of object"""
+    return json.dumps(my_obj)
