e946154dbfe5dab65a54c62451c0bcefdb7afadc
Chidimma <chidimmamofodum@gmail.com>
2023-09-11 22:17:13 -0400

    implementing the area and __str__ function

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""A module that defines a class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialization function
+        Args:
+            width(int): width of rectangle
+            height(int): height of rectangle
+            """
+        Rectangle.integer_validator(self, "width", width)
+        Rectangle.integer_validator(self, "height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """a method that returns area of shape
+        Args:
+            size: size of shape
+        Returns:
+            area of shape
+        """
+
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return f"{[Rectangle.__name__]} {self.__width}/{self.__height}"
