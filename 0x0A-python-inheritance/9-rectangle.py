fdf0547e41a2438014ae1467a7306ee8cf0722e5
Chidimma <chidimmamofodum@gmail.com>
2023-09-12 11:30:58 -0400

    formatting output

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -31,5 +31,9 @@ class Rectangle(BaseGeometry):
         return (self.__width * self.__height)
 
     def __str__(self):
-        return f"{[self.__class__.__name__]} {self.__width}/{self.__height}"
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
+r = Rectangle(3, 5)
+
+print(r)
+print(r.area())
