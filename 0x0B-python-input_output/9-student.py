04639c198894bc184c8c0f8d5511c99543cdc63b
Chidimma <chidimmamofodum@gmail.com>
2023-09-12 23:27:51 -0400

    student to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A module that defines a student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialization function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """converts attributes to json
+            Args:
+                attrs: attributes to be converted to JSON
+            Returns:
+                attribute in json format
+        """
+        result = {}
+        if attrs is None:
+            attrs = [i for i in dir(self) if i[:2] != '__'
+                     and not callable(getattr(self, i))]
+        for i in attrs:
+            try:
+                result[i] = getattr(self, i)
+            except:
+                pass
+        return result
