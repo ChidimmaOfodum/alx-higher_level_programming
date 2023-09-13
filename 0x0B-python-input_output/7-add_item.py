269579eba92920568fa58fc12eaf56f00a9706be
Chidimma <chidimmamofodum@gmail.com>
2023-09-12 21:43:59 -0400

    docs and making file executable

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" A script that adds all arguments to a Python list & saves to a file"""
+
 from sys import argv
 saveJson = __import__('5-save_to_json_file').save_to_json_file
 loadJson = __import__('6-load_from_json_file').load_from_json_file
