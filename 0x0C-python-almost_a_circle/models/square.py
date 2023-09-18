dcbfa7eee9236becd87cb68349629d1d76b1553e
Chidimma <chidimmamofodum@gmail.com>
2023-09-18 19:57:29 -0400

    docs update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,12 +5,12 @@ from rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A square class that inherits from rectangle"""
+    """A square class that inherits from rectangle class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization function"""
+        """A function that initializes instances of a square"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """A custom __str__ function"""
+        """A custom __str__ function, prints with a specific format"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
