2b275cc1bc7645a5c129bd4c3e5cb7af19450bba
Chidimma <chidimmamofodum@gmail.com>
2023-09-18 16:45:16 -0400

    Base test methods

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,47 @@
+import unittest
+from models.base import Base
+
+class TestBaseMethods(unittest.TestCase):
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_id(self):
+        b1 = Base(1)
+        self.assertEqual(b1.id, 1)
+    
+    def test_default_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+    
+    def test_nb_objects(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+    
+    def test_id_multiple(self):
+        b1 = Base()
+        b2 = Base(80)
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 80)
+        self.assertEqual(b3.id, 2)
+    
+    def test_no_of_input(self):
+        with self.assertRaises(TypeError):
+            b1 = Base(2,2)
+    
+    def test_private_attr(self):
+        b1 = Base()
+        with self.assertRaises(AttributeError):
+            b1.__nb_objects
+
+
+
+
+
+
+
