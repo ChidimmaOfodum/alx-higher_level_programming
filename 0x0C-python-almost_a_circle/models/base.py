e530c730023f947216ecb0aee4acc2a0e8b51b88
Chidimma <chidimmamofodum@gmail.com>
2023-09-18 21:38:58 -0400

    fixing error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Module containing the ``Base`` class definition."""
 import json
 
+
 class Base:
     """``Base`` class definition."""
     __nb_objects = 0
@@ -13,10 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns a json representation of input"""
-        if (list_dictionaries is None and len(list_dictionaries) == 0):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
