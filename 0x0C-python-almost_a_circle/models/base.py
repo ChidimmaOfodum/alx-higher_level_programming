0b2c34025be822eaf223e67f8074683e2a4b2602
Chidimma <chidimmamofodum@gmail.com>
2023-09-18 21:34:55 -0400

    more validation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns a json representation of input"""
-        if (list_dictionaries is None):
-            return []
+        if (list_dictionaries is None and len(list_dictionaries) == 0):
+            return "[]"
         return json.dumps(list_dictionaries)
