7134e2c374c79067c2932826109025adc312678b
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-10-17 09:01:57 -0700

    error fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,6 @@
 -- list number of records with the same score
 
-SELECT score, count(score) AS "number" FROM second_table
+SELECT score, count(*) AS `number` FROM second_table
 	GROUP BY score
-	HAVING count(score) > 1
 	ORDER BY score DESC
 
