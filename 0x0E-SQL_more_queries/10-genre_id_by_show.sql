a64ce11751c5064b3f8b0a94b3387b72e3d13f9a
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-10-18 12:15:44 -0700

    selecting genre_id by tvshow

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- selecting genre id by tv_show
+
+SELECT tv_shows.title, tv_show_genres.genre_id 
+	FROM tv_show_genres INNER JOIN tv_shows 
+	ON tv_show_genres.show_id = tv_shows.id 
+	ORDER BY tv_shows.title , tv_show_genres.genre_id ASC;
+
