6ba842fb09cd9700baaad2086e23009f34f18e55
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-10-18 10:24:33 -0700

    selecting from multiple tables

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- list all cities of california
 
-SELECT * FROM cities WHERE state_id =
+SELECT id, name FROM cities WHERE state_id =
 	(
 		SELECT id from states where name = 'California'
-	);
+	)
+	ORDER BY cities.id ASC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- selecting fields from multiple tables
+
+SELECT cities.id, cities.name, states.name 
+	FROM cities, states ORDER BY cities.id ASC;
