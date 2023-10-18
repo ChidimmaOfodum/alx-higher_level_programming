d3d9f2857c7ec491b4fa68ac2ea6a5b2290cc388
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-10-18 10:29:06 -0700

    using inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- selecting fields from multiple tables
 
 SELECT cities.id, cities.name, states.name 
-	FROM cities, states ORDER BY cities.id ASC;
+	FROM cities INNER JOIN states
+        ON cities.state_id = states.id	
+	ORDER BY cities.id ASC;
