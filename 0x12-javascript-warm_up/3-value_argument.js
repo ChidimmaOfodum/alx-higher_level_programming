dff8869f83eddefdeae84d5411bdb2184945d02c
Chidimma <chidimmamofodum@gmail.com>
2023-11-13 17:45:45 -0500

    hot fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 const argc = process.argv.length;
-if (argc <= 2) console.log("No argument");
-else console.log(process.argv[2]);
+if (argc <= 2)
+    console.log('No argument');
+else
+    console.log(process.argv[2]);
