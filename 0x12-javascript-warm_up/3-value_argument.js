a2f72250dbb1805b7574c5336b05745f73833362
Chidimma <chidimmamofodum@gmail.com>
2023-11-13 18:00:44 -0500

    refactor

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const argc = process.argv.length;
-if (argc <= 2)
-    console.log('No argument');
-else
-    console.log(process.argv[2]);
+if (argc <= 2) console.log('No argument');
+else console.log(process.argv[2]);
