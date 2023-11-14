d8f94ee1c67755d7c9e089ccdd42caa6290b7604
Chidimma <chidimmamofodum@gmail.com>
2023-11-13 21:10:09 -0500

    removing duplicate

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,7 @@
 const { argv } = require('process');
 if (argv.length === 2 || argv.length === 3) console.log(0);
 else {
-  const numbers = argv.slice(2).sort();
-  console.log(parseInt(numbers[numbers.length - 2]));
+  let numbers = argv.slice(2).sort();
+  numbers = numbers.filter((x, i) => numbers.lastIndexOf(x) === i);
+  console.log(parseInt(numbers[numbers.length - 2]) || parseInt(numbers[0]));
 }
