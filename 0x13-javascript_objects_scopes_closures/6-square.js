bd656cea4f5c3ebf288f12ff50be16e948d3b883
Chidimma <chidimmamofodum@gmail.com>
2023-11-14 21:38:43 -0500

    hot fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const BaseSquare = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends BaseSquare {
   charPrint (c) {
     const char = c === undefined ? 'X' : c;
 
@@ -17,4 +13,6 @@ module.exports = class Square extends Rectangle {
       console.log(result);
     }
   }
-};
+}
+
+module.exports = Square;
