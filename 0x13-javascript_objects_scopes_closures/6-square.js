e5c35c7eebdfd8c7044822f05456e3ba1e30db01
Chidimma <chidimmamofodum@gmail.com>
2023-11-14 21:09:11 -0500

    charprint method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    const char = c || 'X';
+
+    for (let i = 0; i < this.height; i++) {
+      let result = '';
+      for (let j = 0; j < this.width; j++) {
+        result += char;
+      }
+      console.log(result);
+    }
+  }
+};
