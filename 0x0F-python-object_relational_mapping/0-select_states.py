b5d63bdf85157242a9d371dd04fd578a753e5e9d
Chidimma <chidimmamofodum@gmail.com>
2023-11-18 02:36:35 -0500

    preventing code from executing if imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,16 @@
 import sys
 import MySQLdb
 
-(_, username, password, dbname) = sys.argv
-db = MySQLdb.connect(
-    host='localhost', passwd=password, port="3306", user=username, db=dbname
-)
+if __name__ == "__main__":
+    (_, username, password, dbname) = sys.argv
+    db = MySQLdb.connect(
+        host='localhost', passwd=password,
+        port="3306", user=username, db=dbname
+    )
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-for entry in rows:
-    print(entry)
+    for entry in rows:
+        print(entry)
