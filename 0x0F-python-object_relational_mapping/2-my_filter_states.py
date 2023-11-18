4f1dc69d67d2d8f10f8f3139ffd8af4168133bbb
Chidimma <chidimmamofodum@gmail.com>
2023-11-18 09:38:25 -0500

    preventing script from executing when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,14 +3,15 @@
 import sys
 import MySQLdb
 
-(_, username, password, dbname, state) = sys.argv
-port = 3306
-db = MySQLdb.connect(
-    host='localhost', passwd=password,
-    port=port, user=username, db=dbname
-)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name = %s", [state])
-rows = cur.fetchall()
-for entry in rows:
-    print(entry)
+if __name__ == "__main__":
+    (_, username, password, dbname, state) = sys.argv
+    port = 3306
+    db = MySQLdb.connect(
+        host='localhost', passwd=password,
+        port=port, user=username, db=dbname
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", [state])
+    rows = cur.fetchall()
+    for entry in rows:
+        print(entry)
