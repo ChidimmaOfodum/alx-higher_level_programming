fec7fc1494aa4f9c61e501b640d913901b4bd61c
Chidimma <chidimmamofodum@gmail.com>
2023-11-18 02:39:04 -0500

    docs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# A script that connects to a mysql db and retrieves all states
+"""A script that connects to a mysql db and retrieves all states"""
 import sys
 import MySQLdb
 
