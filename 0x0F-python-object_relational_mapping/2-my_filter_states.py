3138c917c31cfc8aaba01231a3ab5286fdf0d075
Chidimma <chidimmamofodum@gmail.com>
2023-11-24 19:39:50 -0500

    using format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """A script that filters state by user input"""
 import sys
 import MySQLdb
@@ -11,7 +11,9 @@ if __name__ == "__main__":
         port=port, user=username, db=dbname
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s", [state])
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' \
+            ORDER BY states.id".format(state))
     rows = cur.fetchall()
     for entry in rows:
         print(entry)
