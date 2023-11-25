3f4c2459dfc302ce6ba1696fecbbf5df72c58416
Chidimma <chidimmamofodum@gmail.com>
2023-11-24 20:27:57 -0500

    lists all cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that filters state by user input"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    (_, username, password, dbname) = sys.argv
+    port = 3306
+    db = MySQLdb.connect(
+        host='localhost', passwd=password,
+        port=port, user=username, db=dbname
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id")
+    rows = cur.fetchall()
+    for entry in rows:
+        print(entry)
