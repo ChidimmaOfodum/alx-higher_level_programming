e38cb42037b03c97b76b28d43a853dbdce21eb57
Chidimma <chidimmamofodum@gmail.com>
2023-11-24 19:49:48 -0500

    safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that filters state by user input"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    (_, username, password, dbname, state) = sys.argv
+    port = 3306
+    db = MySQLdb.connect(
+        host='localhost', passwd=password,
+        port=port, user=username, db=dbname
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s \
+            ORDER BY states.id", [state])
+    rows = cur.fetchall()
+    for entry in rows:
+        print(entry)
