9fc3bfe12e4ba125bd6cedab71d32a225cc6676c
Chidimma <chidimmamofodum@gmail.com>
2023-12-01 18:23:54 -0500

    fetches data from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# fetches data from a url
+if __name__ == "__main__":
+    import urllib.request as request
+
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
