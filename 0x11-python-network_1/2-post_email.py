3fe7b83f529c7330ecc961a132ea95f8d9078609
Chidimma <chidimmamofodum@gmail.com>
2023-12-01 20:21:25 -0500

    hot fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
 
     req = request.Request(argv[1], data)
     with request.urlopen(req) as response:
-        print(response.read.decode('utf-8'))
+        print(response.read().decode('utf-8'))
