da127e469b894bdf7407a85db1ce9058b0001fcb
Chidimma <chidimmamofodum@gmail.com>
2023-12-01 20:16:40 -0500

    post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Makes a post request"""
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+
+    email = {
+        "email": argv[2]
+    }
+    data = parse.urlencode(email)
+    data = data.encode("ascii")
+
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        print(response.read.decode('utf-8'))
