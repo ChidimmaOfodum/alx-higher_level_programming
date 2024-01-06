cf411eafe9eebe5d1077d6b8bda9e0fc57cbff88
Chidimma <chidimmamofodum@gmail.com>
2024-01-06 18:31:14 -0500

    semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@
 const request = require('request');
 
 if (process.argv[2]) {
-    request(process.argv[2], (err, res, body) => {
-        console.log(res && `code: ${res.statusCode}`);
-    })
+  request(process.argv[2], (_, res, body) => {
+    console.log(res && `code: ${res.statusCode}`);
+  });
 }
