e338526953e8d53c31a0ac0422984b30f47cdd3b
Chidimma <chidimmamofodum@gmail.com>
2024-01-06 16:40:25 -0500

    reading from a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Reading and printing from a file
+
+const fs = require("fs");
+
+if (process.argv[2]) {
+  fs.readFile(process.argv[2], "utf-8", (err, data) => {
+    console.log(data || err);
+  });
+}
