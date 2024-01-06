76e8dffcb20907637bedff8cafb06cc699b2f359
Chidimma <chidimmamofodum@gmail.com>
2024-01-06 18:20:37 -0500

    semistandard stylingg

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Reading and printing from a file
 
-const fs = require("fs");
+const fs = require('fs');
 
 if (process.argv[2]) {
-  fs.readFile(process.argv[2], "utf-8", (err, data) => {
-    console.log(data || err);
+  fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+    console.log(err || data);
   });
 }
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 // Writes to a file
-const fs = require('fs')
+const fs = require('fs');
 
 if (process.argv.length === 4) {
-    fileName = process.argv[2]
-    data = process.argv[3]
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-            console.log(err)
-    })
-   
+  const fileName = process.argv[2];
+  const data = process.argv[3];
+  fs.writeFile(fileName, data, (err) => {
+    if (err) { console.log(err); }
+  });
 }
