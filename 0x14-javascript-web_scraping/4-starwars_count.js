0aa0e3898f79a0955a896478afd008346feffd64
Chidimma <chidimmamofodum@gmail.com>
2024-01-06 19:33:54 -0500

    filtering with id

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,14 +5,13 @@ const request = require('request');
 
 if (process.argv[2]) {
   const url = 'https://swapi-api.alx-tools.com/api/films';
-  const targetCharUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
   request(url, (_, res, body) => {
     if (res && res.statusCode === 200) {
       let count = 0;
       const { results } = JSON.parse(body);
       for (const item of results) {
         const { characters } = item;
-        count = characters.includes(targetCharUrl) ? count + 1 : count;
+        count = characters.find((x) => x.endsWith('/18/')) ? count + 1 : count;
       }
       console.log(count);
     }
