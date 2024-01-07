2be364acf7065caaf4bc78f94ece8ad0c5046020
Chidimma <chidimmamofodum@gmail.com>
2024-01-06 19:22:30 -0500

    fetching data from api:error fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,17 @@
 const request = require('request');
 
 if (process.argv[2]) {
-    const url = "https://swapi-api.alx-tools.com/api/people/18";
-    request(url, (err, res, body) => {
-        console.log(JSON.parse(body).films.length)
-    })
+  const url = 'https://swapi-api.alx-tools.com/api/films';
+  const targetCharUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+  request(url, (_, res, body) => {
+    if (res && res.statusCode === 200) {
+      let count = 0;
+      const { results } = JSON.parse(body);
+      for (const item of results) {
+        const { characters } = item;
+        count = characters.includes(targetCharUrl) ? count + 1 : count;
+      }
+      console.log(count);
+    }
+  });
 }
